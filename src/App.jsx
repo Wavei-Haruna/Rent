5393e367196ea27b9a16ebcca5714833f7667ec6
Wavei Haruna <wavharun2@gmail.com>
2022-12-15 11:44:41 +0000

    Buildning of Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 import ForgetPassword from "./pages/ForgetPassword";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
@@ -9,14 +10,14 @@ import SignUp from "./pages/SignUp";
 function App() {
   return (
     <Router>
-      <h1> Hello Well Come</h1>
+      <Navbar />
       <Routes>
-        <Route exact path="/" element={Home} />
-        <Route exact path="/profile" element={Profile} />
-        <Route exact path="/sign-in" element={SignIn} />
-        <Route exact path="/sign-up" element={SignUp} />
-        <Route exact path="/forget-password" element={ForgetPassword} />
-        <Route exact path="/offers" element={Offers} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/forget-password" element={<ForgetPassword />} />
+        <Route exact path="/offers" element={<Offers />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+useLocation;
+import SimpliRent from "../assets/Images/simpliRent.png";
+
+export default function Navbar() {
+  const Location = useLocation();
+  const Navigate = useNavigate();
+  console.log(Location.pathname);
+  const PathName = (routes) => {
+    if (routes === Location.pathname) return true;
+  };
+  return (
+    <div className=" border-b bg-white  z-50 shadow-sm">
+      <header className=" flex justify-between items-center p-4 max-w-6xl mx-auto ">
+        <div>
+          <img
+            className="h-8 cursor-pointer"
+            src={SimpliRent}
+            alt="Logo"
+            onClick={() => Navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="menu flex  space-x-10 ">
+            <li
+              className={`cursor-pointer py-2 font-semibold text-[#666666] border-b-2 border-b-transparent ${
+                PathName("/") && "border-b-gray-500 text-black"
+              }`}
+              onClick={() => Navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-2 font-semibold text-[#666666] border-b-2 border-b-transparent ${
+                PathName("/offers") && "border-b-gray-500 text-black"
+              }`}
+              onClick={() => Navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-2 font-semibold text-[#666666] border-b-2 border-b-transparent ${
+                PathName("/sign-in") && "border-b-gray-500 text-black"
+              }`}
+              onClick={() => Navigate("/sign-in")}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
